633c83ef1084a4cf68587ffc6c938e785e416913
Mairaj <rayyanmairaj2@gmail.com>
2024-09-19 12:18:47 +0800

    handled displaying half hour marks in the grid

--- frontend-2/src/components/Timetable.js
+++ frontend-2/src/components/Timetable.js
@@ -1,88 +1,81 @@
 import { Fragment } from "react";
-import { sub, format } from "date-fns";
 
 function Timetable({ selectedOccurrences }) {
   const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-  const times = ["8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM", "7:00 PM"];
+  const times = [
+    "8:00 AM", "9:00 AM", "10:00 AM",
+    "11:00 AM", "12:00 PM", "1:00 PM",
+    "2:00 PM", "3:00 PM", "4:00 PM",
+    "5:00 PM", "6:00 PM", "7:00 PM"
+  ];
 
   const convertDayToColumn = (day) => {
-    switch (day) {
-      case "Monday":
-        return 1;
-      case "Tuesday":
-        return 2;
-      case "Wednesday":
-        return 3;
-      case "Thursday":
-        return 4;
-      case "Friday":
-        return 5;
-      default:
-        return 1;
-    }
+    return days.indexOf(day) + 2;
   }
 
   const convertTimeToRow = (time) => {
-    const temp = sub(new Date(`2024-09-18T${time}`), { hours: 8 })
-    return parseInt(format(temp, "H"), 10);
+    const [hour, minutes] = time.split(":").map(Number);
+    const hourOffset = hour - 8;                    // Assuming timetable starts at 8 AM
+    const halfHourOffset = minutes === 30 ? 1 : 0;
+    return 2 + hourOffset * 2 + halfHourOffset + 1; // Calculate grid row
   }
 
-    return (
-      <div className="timetable-grid">
-        {days.map((day, dayIdx) => (
-          <div
-            key={dayIdx}
-            style={{
-              gridColumn: dayIdx + 2,
-              gridRow: 1,
-            }}
-            className="timetable-header"
-          >
-            {day}
-          </div>
-        ))}
-        {times.map((time, timeIdx) => (
-          <div
-            key={timeIdx}
-            style={{
-              gridColumn: 1,
-              gridRow: timeIdx + 2,
-            }}
-            className="time-slot"
-          >
-            {time}
-          </div>
-        ))}
-        {selectedOccurrences.map((occurrence, idx) => {
-          return <Fragment key={idx}>
-            {occurrence.lecture && (
-              <div
-                key={idx + "lecture"}
-                style={{
-                  gridColumn: `${convertDayToColumn(occurrence.lecture.day) + 1}`,
-                  gridRow: `${convertTimeToRow(occurrence.lecture.begin_time) + 2} / ${convertTimeToRow(occurrence.lecture.end_time) + 2}`,
-                }}
-                className="lecture-event"
-              >
-                {occurrence.module} ({occurrence.lecture.begin_time} - {occurrence.lecture.end_time})
-              </div>
-            )}
-            {occurrence.tutorial && (
-              <div
-                key={idx + "tutorial"}
-                style={{
-                  gridColumn: `${convertDayToColumn(occurrence.tutorial.day) + 1}`,
-                  gridRow: `${convertTimeToRow(occurrence.tutorial.begin_time) + 2} / ${convertTimeToRow(occurrence.tutorial.end_time) + 2}`,
-                }}
-                className="tutorial-event"
-              >
-                {occurrence.module} ({occurrence.tutorial.begin_time} - {occurrence.tutorial.end_time})
-              </div>
-            )}
-          </Fragment>
-        })}
-      </div>
-    );
-  }
+  return (
+    <div className="timetable-grid">
+      {days.map((day, dayIdx) => (
+        <div
+          key={dayIdx}
+          style={{
+            gridColumn: dayIdx + 2,
+            gridRow: `${1} / ${3}`,
+          }}
+          className="timetable-header"
+        >
+          {day}
+        </div>
+      ))}
+      {times.map((time, timeIdx) => (
+        <div
+          key={timeIdx}
+          style={{
+            gridColumn: 1,
+            gridRow: `${(timeIdx + 1)*2 + 1} / ${(timeIdx + 1)*2 + 3}`,
+          }}
+          className="time-slot"
+        >
+          {time}
+        </div>
+      ))}
+      {selectedOccurrences.map((occurrence, idx) => {
+        return <Fragment key={idx}>
+          {occurrence.lecture && (
+            <div
+              key={idx + "lecture"}
+              style={{
+                gridColumn: `${convertDayToColumn(occurrence.lecture.day)}`,
+                gridRow: `${convertTimeToRow(occurrence.lecture.begin_time)} / ${convertTimeToRow(occurrence.lecture.end_time)}`,
+              }}
+              className="lecture-event"
+            >
+              {occurrence.module} ({occurrence.lecture.begin_time} - {occurrence.lecture.end_time})
+            </div>
+          )}
+          {occurrence.tutorial && (
+            <div
+              key={idx + "tutorial"}
+              style={{
+                gridColumn: `${convertDayToColumn(occurrence.tutorial.day)}`,
+                gridRow: `${convertTimeToRow(occurrence.tutorial.begin_time)} / ${convertTimeToRow(occurrence.tutorial.end_time)}`,
+              }}
+              className="tutorial-event"
+            >
+              {occurrence.module} ({occurrence.tutorial.begin_time} - {occurrence.tutorial.end_time})
+            </div>
+          )}
+        </Fragment>
+      })}
+    </div>
+  );
+}
 
-  export default Timetable;
+export default Timetable;
