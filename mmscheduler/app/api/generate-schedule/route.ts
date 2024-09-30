b24b3a14859ed91f9aa251df28d0990074e482c1
Mairaj <rayyanmairaj2@gmail.com>
2024-10-01 00:45:31 +0800

    fixed env variables

--- mmscheduler/app/api/generate-schedule/route.ts
+++ mmscheduler/app/api/generate-schedule/route.ts
@@ -2,9 +2,9 @@ import JamAI from "jamaibase";
 import { NextRequest, NextResponse } from "next/server";
 
 const jamai = new JamAI({
-  baseURL: process.env.NEXT_PUBLIC_JAMAI_BASEURL!,
-  apiKey: process.env.NEXT_PUBLIC_JAMAI_API_KEY,
-  projectId: process.env.NEXT_PUBLIC_JAMAI_PROJECT_ID,
+  baseURL: process.env.JAMAI_BASEURL!,
+  apiKey: process.env.JAMAI_API_KEY,
+  projectId: process.env.JAMAI_PROJECT_ID,
 });
 
 export async function POST(request: NextRequest) {
