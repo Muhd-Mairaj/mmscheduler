0aa21b0eb533c65fd93e3aa5dea9982877e0e103
Mairaj <rayyanmairaj2@gmail.com>
2024-10-01 00:57:16 +0800

    configurations for deployment

--- mmscheduler/next.config.mjs
+++ mmscheduler/next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // prevent source maps in build
+    productionBrowserSourceMaps: false,
+
+    // enforce https
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Strict-Transport-Security',
+                        value: 'max-age=31536000; includeSubDomains; preload',
+                    },
+                ],
+            },
+        ];
+    },
+};
 
 export default nextConfig;
