be96e8a1eac95e8f6c64f4dd3e2ee57b538f868e
Mairaj <rayyanmairaj2@gmail.com>
2024-10-02 07:59:29 +0800

    updated to parse data in new format allowing activities of multiple kinds stored in an array of activities fixed bug in parse_module_offering that failed to parse offerings correctly if more than 1 course code appears in one row

--- scraper/main.py
+++ scraper/main.py
@@ -9,7 +9,11 @@ from custom_sheets.MayaSheet import get_sheet as get_maya_sheet
 from custom_sheets.TimeEditSheet import get_sheet as get_time_edit_sheet
 
 """
-timeedit mode currently does not support updating previously scraped data because credits will be added to previous credits
+timeedit mode currently does not support updating previously scraped data.
+this is because of the way credits are calculated and activities are appended
+if the same module is scraped again, the credits will be added to the previous credits
+
+Hence, duplicate modules is currently an issue.
 """
 
 
@@ -23,6 +27,13 @@ def main():
     parser.add_argument("--output", required=True,
                         help="The path to the output json file")
     args = parser.parse_args()
+
+    if args.mode == "timeedit":
+        # reset the output file for directory
+        # updating is not supported in this mode because of the way credits are calculated
+        # if the same module is scraped again, the credits will be added to the previous credits
+        # and the activities will be duplicated
+        json.dump({}, open(args.output, "w"), indent=2)
 
     if args.mode != "finalise" and os.path.isdir(args.path):
         files = [os.path.join(args.path, file) for file in os.listdir(
@@ -81,12 +92,12 @@ def read_data(sheet: TimeEditSheet.MyReadOnlyWorksheet | TimeEditSheet.Worksheet
     for row in sheet.iter_rows(min_row=sheet.header_row+1, max_row=sheet.end_row, values_only=True):
         print(row)
         module_offering = row[sheet._module_offering_column - 1]
-        code, _, occurences = parse_module_offering(module_offering)
+        parsed_dict, period = parse_module_offering(module_offering)
 
         module = row[sheet._module_column - 1]
-        activity = "tutorial" if row[sheet._activity_column -
-                                     1].lower() == "tutorial" else "lecture"
-        # activity = row[sheet._activity_column - 1].lower()
+        # activity = "tutorial" if row[sheet._activity_column -
+        #                              1].lower() == "tutorial" else "lecture"
+        activity = row[sheet._activity_column - 1].lower()
 
         day = row[sheet._day_column - 1]
         begin_time = row[sheet._begin_column - 1]
@@ -94,47 +105,47 @@ def read_data(sheet: TimeEditSheet.MyReadOnlyWorksheet | TimeEditSheet.Worksheet
 
         room = _get_row_item(row, sheet._room_column - 1)
 
-        for occ in occurences:
+        # print(f"{parsed_dict = }")
+        for code, occurences in parsed_dict.items():
             tracker.setdefault(code, {})
             # if code not in tracker:
             #     tracker[code] = {}
+            for occ in occurences:
 
-            # if this occ for this code is not found before, start tracking it
-            module_name = english_name_map[code][occ].get(
-                "module") if english_name_map else module
-            print(f"{module_name=}")
-            tracker[code].setdefault(occ, {
-                "module": module_name or module,
-                "course_id": code,
-                "occurence": occ,
-                "credits": 0,
-            })
-            # if occ not in tracker[code]:
-            #     tracker[code][occ] = {
-            #         "module": module,
-            #         "course_id": code,
-            #         "occurence": occ,
-            #     }
+                # if this occ for this code is not found before, start tracking it
+                try:
+                    module_name = english_name_map[code][occ].get(
+                        "module") if english_name_map else module
+                    # after the change, some codes are not found in the english_name_map
+                    # not sure why, so this is a temporary fix
+                except KeyError:
+                    module_name = module
 
-            if activity not in tracker[code][occ]:
+                print(f"{module_name=}")
+                tracker[code].setdefault(occ, {
+                    "module": module_name or module,
+                    "course_id": code,
+                    "occurence": occ,
+                    "credits": 0,
+                    "activities": [],
+                })
+
+                # update credits for this activity
                 tracker[code][occ]["credits"] += (
                     int(end_time.split(":")[0]) - int(begin_time.split(":")[0]))
 
-            tracker[code][occ].setdefault(activity, {})
-            # if activity not in tracker[code][occ]:
-            #     tracker[code][occ][activity] = {}
+                # add this activity to the list of activities
+                tracker[code][occ]["activities"].append({
+                    "title": activity,
+                    "day": day,
+                    "room": room,
+                    "begin_time": begin_time,
+                    "end_time": end_time,
+                })
 
-            # use set to not overwrite existing data
-            tracker[code][occ][activity].setdefault("day", day)
-            tracker[code][occ][activity].setdefault("room", room)
-            tracker[code][occ][activity].setdefault("begin_time", begin_time)
-            tracker[code][occ][activity].setdefault("end_time", end_time)
-            # tracker[code][occ][activity] = {
-            #     "day": day,
-            #     "room": room,
-            #     "begin_time": begin_time,
-            #     "end_time": end_time,
-            # }
+                # sort activities by title, and then end_time - begin_time
+                tracker[code][occ]["activities"].sort(key=lambda x: (
+                    x["title"], -(int(x["end_time"].split(":")[0]) - int(x["begin_time"].split(":")[0]))))
 
     json.dump(tracker, open(output, "w"), indent=2)
 
@@ -160,17 +171,10 @@ def update_data_from_maya(sheet: MayaSheet.MyReadOnlyWorksheet | MayaSheet.Works
         module_name = row[sheet._module_name_column - 1]
         occurrence = row[sheet._occurrence_column - 1]
         mav_name = row[sheet._mav_name_column - 1]
-        activity = row[sheet._activity_column - 1]
+        # activity = row[sheet._activity_column - 1]
         time_details = row[sheet._time_details_column - 1]
-        # print(f"{current_module_code} {
-        #   current_module_name} Occ {current_occurence}")
-        # print(f"{repr(time_details)}")
 
         day, begin_time, end_time = parse_time_details(time_details)
-        # print(f"{day=}")
-        # print(f"{begin_time=}")
-        # print(f"{end_time=}")
-        # print()
 
         tutor = row[sheet._tutor_column - 1]
         room = row[sheet._room_column - 1]
@@ -178,14 +182,14 @@ def update_data_from_maya(sheet: MayaSheet.MyReadOnlyWorksheet | MayaSheet.Works
         if not current_module_code or (module_code):
             current_module_code = module_code
             current_module_name = module_name
-            module_code = None   # some random string that wont match the next find
-            module_name = None   # some random string that wont match the next find
+            module_code = None   # reset variable for next iterations
+            module_name = None   # reset variable for next iterations
 
         if not current_occurence or (occurrence):
             current_occurence = occurrence
             current_mav_name = mav_name
-            occurrence = None   # some random string that wont match the next find
-            mav_name = None   # some random string that wont match the next find
+            occurrence = None   # reset variable for next iterations
+            mav_name = None   # reset variable for next iterations
 
         if current_module_code not in tracker:
             print(f"Module '{current_module_code} {
@@ -206,29 +210,12 @@ def update_data_from_maya(sheet: MayaSheet.MyReadOnlyWorksheet | MayaSheet.Works
                   current_module_code} {current_module_name}'")
             continue
 
-        # update lecture details if present
-        if lecture_info := tracker[current_module_code][current_occurence].get("lecture"):
-            if lecture_info["day"].lower() == day.lower() and \
-                lecture_info["begin_time"] == begin_time and \
-                    lecture_info["end_time"] == end_time:
+        for activity in tracker[current_module_code][current_occurence].get("activities", []):
+            if activity["day"].lower() == day.lower() and \
+                activity["begin_time"] == begin_time and \
+                    activity["end_time"] == end_time:
                 # this will update original because its a reference
-                lecture_info["tutor"] = tutor
-
-        # update online details if present
-        if online_info := tracker[current_module_code][current_occurence].get("online"):
-            if online_info["day"].lower() == day.lower() and \
-                online_info["begin_time"] == begin_time and \
-                    online_info["end_time"] == end_time:
-                # this will update original because its a reference
-                online_info["tutor"] = tutor
-
-        # update tutorial details if present
-        if tutorial_info := tracker[current_module_code][current_occurence].get("tutorial"):
-            if tutorial_info["day"].lower() == day.lower() and \
-                tutorial_info["begin_time"] == begin_time and \
-                    tutorial_info["end_time"] == end_time:
-                # this will update original because its a reference
-                tutorial_info["tutor"] = tutor
+                activity["tutor"] = tutor
 
     json.dump(tracker, open(output, "w"), indent=2)
 
@@ -245,13 +232,20 @@ def parse_time_details(time_details: str):
 def parse_module_offering(module_offering: str) -> tuple:
     offerings = module_offering.split(", ")
     if len(offerings) != 1:
-        code = offerings[0].split("/")[0]
+        parsed_dict = {}
         period = offerings[0].split("/")[-2]
-        occs = [offering.split("/")[-1] for offering in offerings]
-        return code, period, occs
+        for offering in offerings:
+            code = offering.split("/")[0]
+            parsed_dict.setdefault(code, [])
+            parsed_dict[code].append(offering.split("/")[-1])
+
+        return parsed_dict, period
 
     parsed = module_offering.split("/")
-    return parsed[0], parsed[-2], [parsed[-1]]
+    parsed_dict = {
+        parsed[0]: [parsed[-1]]
+    }
+    return parsed_dict, parsed[-2]
 
 
 def convert_tracking_data_to_output_format(tracking_data):
