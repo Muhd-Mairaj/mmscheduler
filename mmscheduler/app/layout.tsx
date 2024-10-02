7d656fd30a2063c4f4df7137412813a1d108ae4a
Mohammed-AlSharafi <mohammednyemen@gmail.com>
2024-10-02 10:45:53 +0800

    added vercel analytics, added different colors to different activity types, added lecture titles to occ cards and timetable cards

--- mmscheduler/app/layout.tsx
+++ mmscheduler/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { Fredoka } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/react";
 
 const fredoka = Fredoka({
   subsets: ['latin'],
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en" className={fredoka.className}>
       <body>
         {children}
+        <Analytics />
       </body>
     </html>
   );
