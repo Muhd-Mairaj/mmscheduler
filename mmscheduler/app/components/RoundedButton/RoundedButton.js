e4db4ed2cbe39dd4fd1a7c403c719ddfbd582e7d
Mohammed-AlSharafi <mohammednyemen@gmail.com>
2025-02-25 22:37:08 +0800

    fixed bug where two clicks where needed to expand a course

--- mmscheduler/app/components/RoundedButton/RoundedButton.js
+++ mmscheduler/app/components/RoundedButton/RoundedButton.js
@@ -1,9 +1,9 @@
 import classes from "./RoundedButton.module.css";
 import { Button } from "reactstrap";
 
-const RoundedButton = ({ children, onClick, className }) => {
+const RoundedButton = ({ children, onClick, className, disabled=false }) => {
   return (
-    <Button className={`${classes.roundedButton} ${className}`} onClick={onClick}>
+    <Button className={`${classes.roundedButton} ${className}`} onClick={onClick} disabled={disabled}>
       {children}
     </Button>
   );
